915b50d49b05eeb99c109d18fa4ed9bbc7904056
alberto9-73 <127358560+alberto9-73@users.noreply.github.com>
2023-11-06 21:07:43 -0300

    FIX ENV

--- src/db/dbcontex.ts
+++ src/db/dbcontex.ts
@@ -1,17 +1,14 @@
 
 import { DataSource } from "typeorm";
 
-
-
-
 export const dbcontext = new DataSource({
 	type: 'mariadb',
+	host: process.env.DB_HOST,
 	logging:false,
 	synchronize:false,
-	host:'127.0.0.1',
-	port:3306,
+	port:  Number(process.env.DB_PORT)|| 3306,
 	username:'root',
 	password:'123456',
-	database:  'blog',
+	database:process.env.DB_NAME,
 	entities: [__dirname + '/../**/*.entity.{js,ts}'],
 });
